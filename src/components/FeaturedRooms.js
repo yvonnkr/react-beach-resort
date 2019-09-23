36843a8b99d8f4bcadb6e920694a4a878beb894d
Yvonne Nkr <yvonnkr86@gmail.com>
2019-09-24 00:01:33 +0100

    Setup Loading Component

--- src/components/FeaturedRooms.js
+++ src/components/FeaturedRooms.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
 import { RoomContext } from '../context';
+import Loading from './Loading';
+import Room from './Room';
 
 export default class FeaturedRooms extends Component {
   static contextType = RoomContext;
   render() {
-    const { featuredRooms: rooms } = this.context;
-    console.log(rooms);
+    const { featuredRooms: rooms, loading } = this.context;
 
-    return <h2>featured rooms</h2>;
+    return (
+      <>
+        <h2>Featured rooms</h2>
+        <Room />
+        <Loading />
+      </>
+    );
   }
 }
---
+++ src/components/Room.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Room = () => {
+  return (
+    <div>
+      <h2>Hello from room</h2>
+    </div>
+  );
+};
+
+export default Room;
