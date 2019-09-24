8e5300f104b08e1b1a5decb218f1fa10a8182f76
Yvonne Nkr <yvonnkr86@gmail.com>
2019-09-24 20:39:35 +0100

    Single room setup

--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { Link } from 'react-router-dom';
 import defaultBcg from '../images/room-1.jpeg';
 import Hero from '../components/Hero';
@@ -46,13 +46,47 @@ export default class SingleRoom extends Component {
       images
     } = room;
     return (
-      <StyledHero img={images[0] || this.state.defaultBcg}>
-        <Banner title={`${name} room`}>
-          <Link to='/rooms' className='btn-primary'>
-            back to rooms
-          </Link>
-        </Banner>
-      </StyledHero>
+      <Fragment>
+        <StyledHero img={images[0] || this.state.defaultBcg}>
+          <Banner title={`${name} room`}>
+            <Link to='/rooms' className='btn-primary'>
+              back to rooms
+            </Link>
+          </Banner>
+        </StyledHero>
+        <section className='single-room'>
+          <div className='single-room-images'>
+            {images.map((item, index) => (
+              <img key={index} src={item} alt={name} />
+            ))}
+          </div>
+          <div className='single-room-info'>
+            <article className='desc'>
+              <h3>details</h3>
+              <p>{description}</p>
+            </article>
+            <article className='info'>
+              <h3>info</h3>
+              <h6>price : £{price}</h6>
+              <h6>size {size} SQFT</h6>
+              <h6>
+                max capacity:{' '}
+                {capacity > 1 ? `${capacity} people` : `${capacity} person`}
+              </h6>
+              <h6>{pets ? 'pets allowed' : 'no pets allowed'}</h6>
+              <h6>{breakfast && 'free breakfast included'}</h6>
+            </article>
+          </div>
+        </section>
+        <section className='room-extras'>
+          <h6>extras</h6>
+          <ul className='extras'>
+            {extras.map((item, index) => (
+              <li key={index}>{item}</li>
+            ))}
+          </ul>
+        </section>
+      </Fragment>
     );
   }
 }
