b78b30b2bdefe7b2f614f2435d0f42089a225ead
Yvonne Nkr <yvonnkr86@gmail.com>
2019-09-24 09:51:39 +0100

    Setup Featured Rooms Component

--- src/components/FeaturedRooms.js
+++ src/components/FeaturedRooms.js
@@ -2,18 +2,22 @@ import React, { Component } from 'react';
 import { RoomContext } from '../context';
 import Loading from './Loading';
 import Room from './Room';
+import Title from './Title';
 
 export default class FeaturedRooms extends Component {
   static contextType = RoomContext;
   render() {
-    const { featuredRooms: rooms, loading } = this.context;
+    let { featuredRooms: rooms, loading } = this.context;
+
+    rooms = rooms.map(room => <Room key={room.id} room={room} />);
 
     return (
-      <>
-        <h2>Featured rooms</h2>
-        <Room />
-        <Loading />
-      </>
+      <section className='featured-rooms'>
+        <Title title='featured rooms' />
+        <div className='featured-rooms-center'>
+          {loading ? <Loading /> : rooms}
+        </div>
+      </section>
     );
   }
 }
