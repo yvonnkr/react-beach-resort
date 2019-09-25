a352cf0a89e64b198ee03a8d47bddfc6dd671677
Yvonne Nkr <yvonnkr86@gmail.com>
2019-09-25 10:29:31 +0100

    Setup room list

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import Room from './Room';
 
-const RoomList = () => {
+const RoomList = ({ rooms }) => {
+  if (rooms.length === 0) {
+    return (
+      <div className='empty-search'>
+        <h3>unfortunately no rooms matched your search parameters</h3>
+      </div>
+    );
+  }
+
   return (
-    <div>
-      <h3>hello from rooms list</h3>
-    </div>
+    <section className='roomslist'>
+      <div className='roomslist-center'>
+        {rooms.map(item => (
+          <Room key={item.id} room={item} />
+        ))}
+      </div>
+    </section>
   );
 };
 
