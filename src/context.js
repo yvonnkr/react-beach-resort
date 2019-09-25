ca89b2a7b95b02ac56a79945b9d711d32a031549
Yvonne Nkr <yvonnkr86@gmail.com>
2019-09-25 13:33:16 +0100

    Setup size and extras filters

--- src/context.js
+++ src/context.js
@@ -60,7 +60,7 @@ class RoomProvider extends Component {
 
   handleChange = e => {
     const target = e.target;
-    const value = e.type === 'checkbox' ? target.checked : target.value;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
     const name = e.target.name;
 
     this.setState(
@@ -102,6 +102,20 @@ class RoomProvider extends Component {
 
     //filter by price
     tempRooms = tempRooms.filter(room => room.price <= price);
+
+    //filter by size
+    tempRooms = tempRooms.filter(
+      room => room.size >= minSize && room.size <= maxSize
+    );
+
+    //filter by breakfast
+    if (breakfast) {
+      tempRooms = tempRooms.filter(room => room.breakfast === true);
+    }
+    //filter by pets
+    if (pets) {
+      tempRooms = tempRooms.filter(room => room.pets === true);
+    }
 
     //change state
     this.setState({
