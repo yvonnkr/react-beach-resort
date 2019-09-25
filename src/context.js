ab1dccd0f023698b3ee125cff09213df10833e81
Yvonne Nkr <yvonnkr86@gmail.com>
2019-09-25 16:36:22 +0100

    Setup Contentful

--- src/context.js
+++ src/context.js
@@ -1,5 +1,12 @@
 import React, { Component } from 'react';
 import items from './data';
+import Client from './Contentful';
+
+// Client.getEntries({
+//   content_type: 'beachResortRoom'
+// })
+//   .then(response => console.log(response.items))
+//   .catch(console.error);
 
 const RoomContext = React.createContext();
 
@@ -20,7 +27,39 @@ class RoomProvider extends Component {
     pets: false
   };
 
+  //getData from contentful
+  getData = async () => {
+    try {
+      let response = await Client.getEntries({
+        content_type: 'beachResortRoom'
+      });
+
+      let rooms = this.formatData(response.items);
+      let featuredRooms = rooms.filter(room => room.featured === true);
+      let maxPrice = Math.max(...rooms.map(item => item.price));
+      let maxSize = Math.max(...rooms.map(item => item.size));
+
+      this.setState({
+        rooms,
+        featuredRooms,
+        sortedRooms: rooms,
+        loading: false,
+        price: maxPrice,
+        maxPrice,
+        maxSize
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   componentDidMount() {
+    this.getData();
+  }
+
+  /** Using local data
+  componentDidMount() {
+    
     let rooms = this.formatData(items);
     let featuredRooms = rooms.filter(room => room.featured === true);
     let maxPrice = Math.max(...rooms.map(item => item.price));
@@ -36,11 +75,9 @@ class RoomProvider extends Component {
       maxSize
     });
   }
-
-  //getData
+   */
 
   formatData(items) {
-    //this.getData
     let tempItems = items.map(item => {
       let id = item.sys.id;
       let images = item.fields.images.map(image => image.fields.file.url);
